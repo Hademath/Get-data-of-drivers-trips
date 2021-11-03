5dd74ebf363539f5ad5e804bc720dff2c6894c17
hademath <hademath1992@gmail.com>
2021-11-03 16:08:58 +0100

    task 3 commited

--- src/analysis.js
+++ src/analysis.js
@@ -1,13 +1,138 @@
-const { getTrips } = require('api');
-
+const { getTrips, getDriver, getVehicle } = require('api');
 /**
  * This function should return the trip data analysis
  *
  * Question 3
  * @returns {any} Trip data analysis
  */
-async function analysis() {
-  // Your code goes here
+
+function stringToNumber(arr){
+    return parseFloat(arr.toString().replace(',', ''))
 }
+
+
+
+async function analysis() {
+  //this part declare getTrips await
+let result;
+  try{
+    result = await getTrips() 
+     //console.log(result)
+  }catch( err ){
+    console.log(new Error('error')); 
+  }
+  
+// this part declare the getDriver promise all await
+
+for(let i=0; i<result.length; i++){
+  //console.log(result[i].driverID)
+  const drivers = {};
+async function getDrivers() {
+  try {
+    driverData = await getDriver(result[i].driverID)
+  } catch (err) {
+    console.log(err.message)
+  }
+
+}
+
+getDrivers();
+
+
+}
+
+
+
+
+/// declare object for the parameter
+
+let objectTemplate = {
+  noOfCashTrips: 0, 
+  noOfNonCashTrips: 0,
+  billedTotal: 0,
+  cashBilledTotal: 0,
+  nonCashBilledTotal: 0,
+  noOfDriversWithMoreThanOneVehicle: 0,
+  mostTripsByDriver: {
+    name: "",
+    email: "",
+    phone: "",
+    noOfTrips: 0,
+    totalAmountEarned: 0
+  },
+  highestEarningDriver: {
+    name: "",
+    email: "",
+    phone: "",
+    noOfTrips: 0,
+    totalAmountEarned: 0
+  }
+
+}
+
+//  1. number of cash trips  
+for(let i=0; i<result.length; i++ ){
+  if(result[i]['isCash']== true){
+    objectTemplate.noOfCashTrips++
+  }
+}
+// return objectTemplate.noOfCashTrips
+console.log(objectTemplate.noOfCashTrips)
+
+// 2. number of Non-cash trips
+
+for(let i=0; i<result.length; i++){
+  if(result[i]['isCash'] == false){
+    objectTemplate.noOfNonCashTrips++
+  }
+}
+// return objectTemplate.noOfNonCashTrips
+ console.log(objectTemplate.noOfNonCashTrips);
+
+// 3. Total bill for both the cash and non-cash trips 
+for(let i=0; i<result.length; i++){
+  if((result[i]['isCash'] == true) || (result[i]['isCash']==false)){
+   objectTemplate.billedTotal += stringToNumber(result[i].billedAmount)
+   roundUp = objectTemplate.billedTotal.toFixed(2)
+  }
+}
+// return   objectTemplate.billedTotal
+console.log( roundUp);
+
+
+
+// 4. Total bill for the noncash trips
+
+for(let i=0; i<result.length; i++){
+  if(result[i]['isCash'] == false){
+        objectTemplate.nonCashBilledTotal += stringToNumber(result[i].billedAmount)
+        roundUp =objectTemplate.nonCashBilledTotal.toFixed(2) 
+  }
+}
+// return objectTemplate.nonCashBilledTotal
+console.log(roundUp)
+
+
+
+///5.  Total billed for the cash trips
+
+for(let i=0; i<result.length; i++){
+  if(result[i]['isCash'] == true)
+   objectTemplate.cashBilledTotal += stringToNumber(result[i].billedAmount)
+
+}
+
+// return objectTemplate.cashBilledTotal
+ console.log(objectTemplate.cashBilledTotal);
+
+ 
+
+
+}
+ 
+ //return objectTemplate
+
+analysis() 
+ 
 
 module.exports = analysis;
--- src/report.js
+++ src/report.js
@@ -1,5 +1,4 @@
-const { getTrips } = require('api');
-
+const { getTrips, getDriver, getVehicle } = require('api');
 /**
  * This function should return the data for drivers in the specified format
  *
@@ -9,6 +8,45 @@ const { getTrips } = require('api');
  */
 async function driverReport() {
   // Your code goes here
+  const trips = await getTrips();
+  const drivers = {};
+  await Promise.all(Object.keys(drivers).map(async driverID=>{
+    try{
+      const details = await getDriver(driverID);
+      drivers[driverID].fullname = details.name;
+      drivers[driverID].id = driverID;
+      drivers[driverID].phone = details.phone;
+      drivers[driverID].vehicles = [];
+      await Promise.all(details.vehicleID.map(async vehicleID=>{
+        const vehicle = await getVehicle(vehicleID);
+        drivers[driverID].vehicles.push({
+          "plate": vehicle.plate,
+          "manufacturer": vehicle.manufacturer
+        });
+      }))
+    } catch (error){}
+  }));
+  trips.forEach(trip=>{
+    if(!drivers[trip.driverID]) drivers[trip.driverID] = {
+      noOfCashTrips:0, noOfNonCashTrips:0, totalAmountEarned:0, totalCashAmount:0, totalNonCashAmount:0, trips:[]};
+      if(typeof trip.billedAmount === "string") trip.billedAmount = parseFloat(trip.billedAmount.replace(/,/g,'')),
+      drivers[trip.driverID].totalAmountEarned += trip.billedAmount;
+      if(trip.isCash){
+        drivers[trip.driverID].noOfCashTrips += 1;
+        drivers[trip.driverID].totalCashAmount += trip.billedAmount;
+      }else{
+        drivers[trip.driverID].noOfNonCashTrips += 1;
+        drivers[trip.driverID].totalNonCashAmount += trip.billedAmount;
+          }
+    drivers[trip.driverID].trips.push({
+      "user": trip.user.name,
+      "created": trip.created,
+      "pickup": trip.pickup.address,
+      "destination": trip.destination.address,
+      "billed": trip.billedAmount,
+      "isCash": trip.isCash
+    });  
+  });
+  return Object.values(drivers)
 }
-
 module.exports = driverReport;
